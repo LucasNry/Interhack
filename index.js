3b0fbb81493d8d29b525b6f08a7a0b96a4088df8
lucasdanery@usp.br <lucasdanery@usp.br>
2019-08-24 20:09:53 -0300

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 let goUp = document.querySelector(".goUp");
-goUp.addEventListener("click", () => {
-  console.log("Works");
+let signIn = document.querySelector("button");
 
-  window.scrollTo(0, 0);
-});
+try {
+  goUp.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
+} catch (err) {}
+
+const redirect = () => {
+  try {
+    window.location.href =
+      "https://idpcafe.usp.br/idp/profile/SAML2/Redirect/SSO?execution=e2s1";
+  } catch (error) {}
+};
