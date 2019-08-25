9b61957e80fb94d31717f2b156d35a1063bf9fd7
lucasdanery@usp.br <lucasdanery@usp.br>
2019-08-25 11:54:47 -0300

    Final

--- index.js
+++ index.js
@@ -4,30 +4,18 @@ let buttons = document.querySelectorAll(".category-button");
 let url = "http://b2c61bb1.ngrok.io/horizontal-plot.png";
 let api = "https://0d19b8f7.ngrok.io/api/products";
 let container = document.querySelector(".canvas");
-let cardTemplate = `<div class="sector-container">
-<a href="./Página3.html"
-  ><p class="info-titulo">
-    Título
-  </p></a
->
-<p class="info-descricao">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla velit
-  justo, mattis eu efficitur at, sollicitudin non magna. Praesent in
-  egestas nulla. In hac habitasse platea dictumst. Suspendisse at nisi
-  vel velit tempor sollicitudin id vel odio. Fusce aliquet tempor
-  libero nec commodo.
-</p>
-<div class="info-bar">
-  <img class="contest" src="./eye.png" alt="view" /><span>:</span>
-  <span class="view-count">1232</span>
-  <img src="./warning.png" alt="contest" class="contest" /><span
-    >:</span
-  >
-  <span class="contest-counter">322</span>
-</div>
-</div>`;
-let titles = ["", "", ""];
-let description = ["", "", ""];
+let counter = 0;
+let cardContainer = document.querySelector(".cards-container");
+console.log(cardContainer);
+
+let titles = ["EACH", "FFLCH", "Poli", "ICMC", "FEA"];
+let description = [
+  "Escola de artes, ciências e humanidades da Universidade de São Paulo. Localizada na zona leste de São Paulo. Possui 11 cursos de graduação, 4129 alunos de graduação matriculados, e mais de 200 docentes.",
+  "Faculdade de filosofia, letras e ciências humanas da Universidade de São Paulo, localizada no campus Butantã.12821 alunos matriculados, 433 docentes e 5 cursos de graduação.",
+  "Escola Politécnica da Universidade de São Paulo, localizada no campus Butantã. Criada em 1893. Possui 17 cursos de Engenharia, 5057 alunos matriculados e 425 docentes.",
+  "Instituto de Ciências Matemáticas e Computação da Universidade de São Paulo, localizada em São Carlos. Possui 2173 alunos matriculados, 131 docentes e 6 cursos de graduação.",
+  "Faculdade de Economia de Admnistração da Universidade de São Paulo, localizada no campus Butantã. Possui 3775 alunos matriculados, 135 docentes e 4 cursos de graduação."
+];
 
 try {
   goUp.addEventListener("click", () => {
@@ -38,7 +26,7 @@ try {
 const redirect = () => {
   try {
     window.location.href =
-      "https://idpcafe.usp.br/idp/profile/SAML2/Redirect/SSO?execution=e2s1";
+      "https://accounts.google.com/ServiceLogin/identifier?service=CPanel&flowName=GlifWebSignIn&flowEntry=AddSession";
   } catch (error) {}
 };
 buttons.forEach(button => {
@@ -46,34 +34,40 @@ buttons.forEach(button => {
 });
 
 // console.log(fetchData(api));
-
-container.innerHTML += `<div class="data-container">
-<h1 class="data-title">${"Titúlo"}</h1>
+try {
+  container.innerHTML += `<div class="data-container">
+<h1 class="data-title">${titles[0]}</h1>
 <br /><br />
-<img class="chart" src="${url}" alt="chart" />
+<img class="chart" src="./each.png" alt="chart" />
 <br /><br />
 <p class="data-description">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nulla
-  nunc, commodo a faucibus convallis, aliquam ut quam. Ut in
-  vestibulum nulla. Cras rutrum tortor fringilla tellus consectetur
-  consectetur. Phasellus facilisis nibh eget orci sodales, at semper
-  ante suscipit. Maecenas sollicitudin mi vitae urna aliquam
-  venenatis. Vestibulum rhoncus turpis sit amet pellentesque
-  dignissim. Phasellus non dui sodales, ultricies lectus nec,
-  sollicitudin lorem. Phasellus a risus non tortor tincidunt elementum
-  quis porttitor dui. Curabitur ornare, augue et viverra scelerisque,
-  leo enim vulputate libero, eu cursus lacus ligula vel eros. Nunc
-  purus ante, gravida ut gravida ut, cursus ac nisi. Vestibulum
-  lobortis elit vel nunc porta sagittis. Praesent cursus hendrerit
-  gravida. Nam feugiat arcu quis tincidunt scelerisque. Fusce finibus
-  purus ac leo pretium, vitae scelerisque risus dapibus. Proin eu
-  justo quis odio auctor lobortis eu ut erat. Nulla facilisi. Proin
-  elit elit, ullamcorper ac mollis a, ullamcorper sit amet nisi. Ut
-  interdum odio et mi tristique interdum. Nunc aliquam, risus id
-  condimentum semper, magna sapien varius purus, nec cursus tortor
-  libero id nunc. Quisque tellus mauris, scelerisque non dictum et,
-  ullamcorper non magna. Phasellus maximus a neque ut pulvinar. Aenean
-  sed ante sit amet quam dignissim sodales. Sed varius massa quis
-  elit.
+  ${description[0]}
 </p>
 </div>`;
+} catch (error) {}
+
+titles.forEach(title => {
+  cardContainer.innerHTML += `<div class="sector-container">
+    <a href="./Página3.html"
+      ><p class="info-titulo">
+        ${title}
+      </p><br></a
+    >
+    <p class="info-descricao">
+      ${description[counter]}
+    </p>
+    <div class="info-bar">
+      <img class="contest" src="./eye.png" alt="view" /><span>:</span>
+      <span class="view-count">${Math.floor(
+        Math.random() * (1000 - 800 + 800)
+      )}</span>
+      <img src="./warning.png" alt="contest" class="contest" /><span
+        >:</span
+      >
+      <span class="contest-counter">${Math.floor(
+        Math.random() * (600 - 200 + 200)
+      )}</span>
+    </div>
+  </div>`;
+  counter++;
+});
